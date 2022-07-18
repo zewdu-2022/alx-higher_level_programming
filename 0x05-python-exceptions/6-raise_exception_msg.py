b97921bfd55d46a5408c7b2c62ad1c1449bed7c2
Zewdu Wondimagegn <zewduw@ymail.com>
2022-07-18 04:54:33 -0700

    TASK 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# zewdu <zewduw@ymail.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
