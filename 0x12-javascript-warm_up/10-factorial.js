c059126e0547fd44bd84adf8e6ef5b2ac2f29566
Zewdu-2022 <zewduw@yamil.com>
2022-10-04 11:16:51 +0000

    Corrction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-/**
- * add - Computes the sum of 2 numbers.
- * @param {Number} a - The first number.
- * @param {Number} b - The second number.
- *
- * @returns The sum of the 2 numbers.
- */
-function add (a, b) {
-  return a + b;
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(
-  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-);
+console.log(factorial(Number(process.argv[2])));
